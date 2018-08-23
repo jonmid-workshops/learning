61459880696121b68a6b63b380d6488a1fe28df6
JonMid <jamideros@hotmail.com>
2018-08-23 17:33:25 -0500

    Cambios
    Ingreso de migraciones, factorias y seeds para ingresar datos en las tablas.

--- config/services.php
+++ config/services.php
@@ -35,4 +35,16 @@ return [
         'secret' => env('STRIPE_SECRET'),
     ],
 
+    //socialite
+    'github' => [
+	    'client_id' => env('GITHUB_CLIENT_ID'),         // Your GitHub Client ID
+	    'client_secret' => env('GITHUB_CLIENT_SECRET'), // Your GitHub Client Secret
+	    'redirect' => '/login/github/callback',
+    ],
+    'facebook' => [
+	    'client_id' => env('FACEBOOK_CLIENT_ID'),         // Your facebook Client ID
+	    'client_secret' => env('FACEBOOK_CLIENT_SECRET'), // Your facebook Client Secret
+	    'redirect' => '/login/facebook/callback',
+    ],
+
 ];
