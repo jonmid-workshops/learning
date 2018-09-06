9a3a13c9583e0387a55e5d177615a07f6705af65
JonMid <jamideros@hotmail.com>
2018-09-06 16:55:52 -0500

    Cambios route

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,13 @@
 |
 */
 
+Route::get('login/{driver}', 'Auth\LoginController@redirectToProvider')->name('social_auth');
+Route::get('login/{driver}/callback', 'Auth\LoginController@handleProviderCallback');
+
 Route::get('/', function () {
     return view('welcome');
 });
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
